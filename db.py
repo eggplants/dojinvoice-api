0688785c79b1b22280970009e9f5e1fb32484fcd
eggplants <w10776e8w@yahoo.co.jp>
2020-11-19 04:13:05 +0900

    Update

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import List, Optional
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
@@ -22,7 +22,45 @@ class DojinvoiceDB(object):
     def get_category_list(self):
         Work = self.tables.work
         datalist = self.session.query(Work.category).all()
-        return set(datalist)
+        return [_[0] for _ in list(set(datalist))]
+
+    def get_genre_list(self):
+        Genre = self.tables.genre
+        datalist = self.session.query(Genre.genre).all()
+        return [_[0] for _ in list(set(datalist))]
+
+    def get_rate_filtered_list(self, min_rate: float, max_rate: float,
+                               idlist: Optional[List[str]] = None):
+        _ = [_['work_id'] for _ in self.engine.execute(
+            '''select work_id from option
+            where rating is not null and :min <= rating and :max >= rating ''',
+            min=min_rate, max=max_rate)]
+        return (set(idlist) | set(_) if idlist is not None else _)
+
+    def get_illustrator_list(self):
+        Illustrator = self.tables.illustrator
+        datalist = self.session.query(Illustrator.illustrator).all()
+        return [_[0] for _ in list(set(datalist))]
+
+    def get_musician_list(self):
+        Musician = self.tables.musician
+        datalist = self.session.query(Musician.musician).all()
+        return [_[0] for _ in list(set(datalist))]
+
+    def get_scenario_list(self):
+        Scenario = self.tables.scenario
+        datalist = self.session.query(Scenario.scenario).all()
+        return [_[0] for _ in list(set(datalist))]
+
+    def get_voice_list(self):
+        Voice = self.tables.voice
+        datalist = self.session.query(Voice.voice).all()
+        return [_[0] for _ in list(set(datalist))]
+
+    def get_writer_list(self):
+        Writer = self.tables.writer
+        datalist = self.session.query(Writer.writer).all()
+        return [_[0] for _ in list(set(datalist))]
 
     def idlist_to_data_list(self, idlist: List[str]):
         Work = self.tables.work
@@ -36,15 +74,15 @@ class DojinvoiceDB(object):
 
         return self.session.query(Work).\
             filter(Work.work_id in idlist).\
-            filter(Work.work_id == Genre.work_id).\
-            filter(Work.work_id == Illustrator.work_id).\
-            filter(Work.work_id == Musician.work_id).\
-            filter(Work.work_id == Option.work_id).\
-            filter(Work.work_id == Scenario.work_id).\
-            filter(Work.work_id == Voice.work_id).\
-            filter(Work.work_id == Writer.work_id).\
+            join(Genre, Work.work_id == Genre.work_id).\
+            join(Genre, Work.work_id == Illustrator.work_id).\
+            join(Musician, Work.work_id == Musician.work_id).\
+            join(Option, Work.work_id == Option.work_id).\
+            join(Scenario, Work.work_id == Scenario.work_id).\
+            join(Voice, Work.work_id == Voice.work_id).\
+            join(Writer, Work.work_id == Writer.work_id).\
             all()
 
-    def search(self, params):
-        # 集合を取りidのリストを返す
-        return res
+    # def search(self, params):
+    #     # 集合を取りidのリストを返す
+    #     return res
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+#!/usr/bin/env python
+from os.path import join
 from typing import Any, TypedDict, cast
 
-from flask import Blueprint, Flask, jsonify, request
-from werkzeug.datastructures import ImmutableMultiDict
+from flask import Blueprint, Flask, jsonify, send_from_directory
 
 from db import DojinvoiceDB
 
 DB = DojinvoiceDB()
 app = Flask(__name__)
-v1 = Blueprint("version1", "version1")
+v1 = Blueprint('version1', 'version1')
 
 
 class Params(TypedDict):
@@ -16,53 +17,108 @@ class Params(TypedDict):
     page: int
     keyword: str
     category: str
-    rate: int
+    max_rate: float
+    min_rate: float
     genre: str
     illustrator: str
     musician: str
     scenario: str
     voice: str
     writer: str
+    filter_rate: bool
 
 
 def res_ok(data: Any, status: int) -> Any:
-    return jsonify({'data': data}), status
+    try:
+        data = data
+        json = jsonify({'data': data})
+        return json, status
+    except Exception as e:
+        return {'err': e}, 500
 
 
-def get_params(args: ImmutableMultiDict[Any, Any]):
+def get_params(args) -> Params:
     params = cast(Params, {})
     params['offset'] = args.get('offset', default=20, type=int)
     params['random'] = args.get('random', default=True, type=bool)
     params['page'] = args.get('page', default=1, type=int)
     params['keyword'] = args.get('keyword', default='', type=str)
     params['category'] = args.get('category', default='', type=str)
-    params['rate'] = args.get('rate', default=1, type=int)
-    params['genre'] = args.get('genre', default=1, type=str)
-    params['illustrator'] = args.get('illustrator', default=1, type=str)
-    params['musician'] = args.get('musician', default=1, type=str)
-    params['scenario'] = args.get('scenario', default=1, type=str)
-    params['voice'] = args.get('voice', default=1, type=str)
-    params['writer'] = args.get('writer', default=1, type=str)
+    params['filter_rate'] = args.get('filter_rate', default=False, type=bool)
+    params['max_rate'] = args.get('max_rate', default=5.0, type=float)
+    params['min_rate'] = args.get('min_rate', default=0.0, type=float)
+    params['genre'] = args.get('genre', default='', type=str)
+    params['illustrator'] = args.get('illustrator', default='', type=str)
+    params['musician'] = args.get('musician', default='', type=str)
+    params['scenario'] = args.get('scenario', default='', type=str)
+    params['voice'] = args.get('voice', default='', type=str)
+    params['writer'] = args.get('writer', default='', type=str)
     return params
 
 
-@v1.route("/search", methods=['GET'])
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(join(app.root_path, 'static/img'), 'favicon.ico')
+
+
+@v1.route('/')
 def index():
-    args = request.args
-    params = get_params(args)
-    return res_ok(DB.search(params), 200)
+    return res_ok([str(_) for _ in app.url_map.iter_rules()], 200)
 
 
-@v1.route("/category_list")
+# @v1.route('/search', methods=['GET'])
+# def search():
+#     args = request.args
+#     params = get_params(args)
+#     return res_ok(DB.search(params), 200)
+
+
+@ v1.route('/category')
 def category():
-    return jsonify({'data': DB.get_category_list()}), 200
+    return res_ok(DB.get_category_list(), 200)
 
 
-@v1.route("/chobit_list")
+@ v1.route('/rate')
+def rate():
+    return res_ok(DB.get_rate_filtered_list(1.0, 5.0), 200)
+
+
+@ v1.route('/chobit')
 def chobit():
-    return jsonify({'data': DB.get_exist_chobit_list()}), 200
+    return res_ok(DB.get_exist_chobit_list(), 200)
 
 
-if __name__ == "__main__":
-    app.register_blueprint(v1, url_prefix="/v1")
-    app.run(host='0.0.0.0', debug=True)
+@ v1.route('/illustrator')
+def illustrator():
+    return res_ok(DB.get_illustrator_list(), 200)
+
+
+@ v1.route('/musician')
+def musician():
+    return res_ok(DB.get_musician_list(), 200)
+
+
+@ v1.route('/scenario')
+def scenario():
+    return res_ok(DB.get_scenario_list(), 200)
+
+
+@ v1.route('/voice')
+def voice():
+    return res_ok(DB.get_voice_list(), 200)
+
+
+@ v1.route('/writer')
+def writer():
+    return res_ok(DB.get_writer_list(), 200)
+
+
+@ v1.route('/genre')
+def genre():
+    return res_ok(DB.get_genre_list(), 200)
+
+
+if __name__ == '__main__':
+    app.register_blueprint(v1, url_prefix='/v1')
+    debug = True
+    app.run(host='0.0.0.0', debug=debug)
